7fc79646c9ab08cc378eb7c198232e4a5c2a39ea
mirzakishi <56716591+mirzakishi@users.noreply.github.com>
2021-11-04 22:18:53 +0300

    SQL scripts

--- Postgresql/Querying Data/Select.sql
+++ Postgresql/Querying Data/Select.sql
@@ -1,3 +1,18 @@
--- show you how to query data from a single table.
+-------------------------------------------------------------------------------------------------------------
+-- Select show you how to query data from a single table.
+select first_name,
+       last_name,
+       email
+from
+    customer;
 
-select  * from customer
+-- Using PostgreSQL SELECT statement with concatenation
+select
+    first_name || ' ' || customer.last_name,
+    email
+from
+    customer;
+
+-- The following example uses the SELECT statement with an expression. It omits the FROM clause:
+select 5^2
+--------------------------------------------------------------------------------------------------------------
