e2fd8ac08b53f099f85929746e61302db7bc027c
mirzakishi <56716591+mirzakishi@users.noreply.github.com>
2021-11-07 17:36:55 +0300

    Sel join statement

---
+++ Postgresql/Joining Multiple Tables/Self-Join.sql
@@ -0,0 +1,55 @@
+--------------------------------------------------------------------------------------------------------------------
+-- A self-join is a regular join that joins a table to itself. In practice, you typically
+-- use a self-join to query hierarchical data or to compare rows within the same table.
+
+-- The following statements create the employee table and insert some sample data into the table.
+CREATE TABLE employee (
+	employee_id INT PRIMARY KEY,
+	first_name VARCHAR (255) NOT NULL,
+	last_name VARCHAR (255) NOT NULL,
+	manager_id INT,
+	FOREIGN KEY (manager_id)
+	REFERENCES employee (employee_id)
+	ON DELETE CASCADE
+);
+INSERT INTO employee (
+	employee_id,
+	first_name,
+	last_name,
+	manager_id
+)
+VALUES
+	(1, 'Windy', 'Hays', NULL),
+	(2, 'Ava', 'Christensen', 1),
+	(3, 'Hassan', 'Conner', 1),
+	(4, 'Anna', 'Reeves', 2),
+	(5, 'Sau', 'Norman', 2),
+	(6, 'Kelsie', 'Hays', 3),
+	(7, 'Tory', 'Goff', 3),
+	(8, 'Salley', 'Lester', 3);
+--
+select * from employee
+--
+SELECT
+    e.first_name || ' ' || e.last_name employee,
+    m .first_name || ' ' || m .last_name manager
+FROM
+    employee e
+INNER JOIN employee m ON m .employee_id = e.manager_id
+ORDER BY manager;
+
+-- Comparing the rows with the same table
+--The following query finds all pair of films that have the same length
+
+SELECT
+    f1.title,
+    f2.title,
+    f1.length
+FROM
+    film f1
+INNER JOIN film f2
+    ON f1.film_id <> f2.film_id AND
+       f1.length = f2.length;
+----------------------------------------------------------------------------------------------------------------------
+
+
